ae39be2cc55533261a3f8fb613f3982409ba0cee
nhlee98 <e5slnh9876@gmail.com>
2023-11-24 17:07:08 +0900

    feat: 명령어 1, 2, 3 중 하나를 입력 받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final InputView inputView = new InputView();
 
-    public static int inputTableNumber() {
+    private InputView() {}
+
+    public static InputView getInstance() {
+        return inputView;
+    }
+
+    public int inputCommandNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
         return scanner.nextInt();
     }
